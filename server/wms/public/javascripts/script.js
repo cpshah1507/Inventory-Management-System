f4f6ad5e63371393dc74a5fde31935dce25bd909
cpshah1507 <cpshah1507@gmail.com>
2018-08-05 15:33:31 -0400

    Adding static data table

--- server/wms/public/javascripts/script.js
+++ server/wms/public/javascripts/script.js
@@ -1,3 +1,10 @@
+var tableData = [["1","SKU1","3","Loc-1"],["2","SKU2","5","Loc-2"];
+
 $( function() {
     $( "#tabs" ).tabs();
-  } );
+
+	$('#InventoryTable').DataTable({
+	    data: tableData
+	});
+});
+
