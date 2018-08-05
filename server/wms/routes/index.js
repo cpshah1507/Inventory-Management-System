000e949bc2303dd37d032d73a87fb8e1b3d30f9d
cpshah1507 <cpshah1507@gmail.com>
2018-08-05 14:21:20 -0400

    Testing postgres connection from node.js

--- server/wms/routes/index.js
+++ server/wms/routes/index.js
@@ -7,3 +7,61 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+//var http = require('http');
+//var net = require('net');
+
+// Postgres connection to DB
+var pg = require('pg');
+var conString = "postgres://postgres:admin@127.0.0.1/wms";
+
+pg.defaults.poolSize = 1;
+
+/********************************************************************************************/
+/*********************************** Database Routes*****************************************/
+/********************************************************************************************/
+
+router.get('/wms',function(req,res,next) {
+	// get a pg client from the connection pool
+ 	//this initializes a connection pool
+	//it will keep idle connections open for a (configurable) 30 seconds
+	//and set a limit of 1 (also configurable)
+	pg.connect(conString, function (err, client, done) {
+		var handleError = function (err) {
+		    // no error occurred, continue with the request
+		    if (!err) return false;
+		    if (client) {
+		        done(client);
+		    }
+		    res.writeHead(500, { 'content-type': 'text/plain' });
+		    res.end('An error occurred');
+		    return true;
+		};
+	    // handle an error from the connection
+	    if (handleError(err)) return;	      
+	    done();        
+	    res.render('wms', { title: 'WMS' , dbConnection: 'Successful'});	
+	});  
+});
+
+
+router.get('/ecsconsole',function(req,res,next) {
+	pg.connect(conString, function (err, client, done) {
+		var handleError = function (err) {
+		    // no error occurred, continue with the request
+		    if (!err) return false;
+		    if (client) {
+		        done(client);
+		    }
+		    res.writeHead(500, { 'content-type': 'text/plain' });
+		    res.end('An error occurred');
+		    return true;
+		};
+	    // handle an error from the connection
+	    if (handleError(err)) return;	      
+	    done();     
+
+		res.render('ecsconsole', { title: 'Cortex Console' , dbConnection: 'Successful'});	
+	});  
+});
