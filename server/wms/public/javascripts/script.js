7461f2543c77e026de5747c26017f60b42905925
cpshah1507 <cpshah1507@gmail.com>
2018-08-05 15:34:30 -0400

    Fixing syntax error

--- server/wms/public/javascripts/script.js
+++ server/wms/public/javascripts/script.js
@@ -1,4 +1,4 @@
-var tableData = [["1","SKU1","3","Loc-1"],["2","SKU2","5","Loc-2"];
+var tableData = [["1","SKU1","3","Loc-1"],["2","SKU2","5","Loc-2"]];
 
 $( function() {
     $( "#tabs" ).tabs();
